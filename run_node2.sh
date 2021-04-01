1715a3d99308f5b182bf2a8ada0bfe3731796b2d
Vincent AUBRIOT <vincent3897@gmail.com>
2021-04-01 16:52:52 +0200

    Physical network creation OK

--- run_node2.sh
+++ run_node2.sh
@@ -1,3 +1,3 @@
 export CP=.:./classes:./lib/amqp-client-5.8.0.jar:./lib/slf4j-api-1.7.30.jar:./lib/slf4j-simple-1.7.30.jar
 export CHAT_SERVER_ADDR=localhost
-java -cp $CP Node data/2nodes.conf 1 $CHAT_SERVER_ADDR
+java -cp $CP Node data/2nodes.conf 0 $CHAT_SERVER_ADDR
--- src/Node.java
+++ src/Node.java
@@ -18,8 +18,9 @@ public class Node {
     static int physicalId;
     static int virtualId;
     public static void main(String[] args) {
-        if (args.length != 2) {
-            System.out.println("Please provide the network configuration and the physical id of this node in order to start the program.");
+        System.out.println(args.length);
+        if (args.length != 3) {
+            System.out.println("Please provide the network configuration, the physical id of this node as well as the RabbitMQ server address to join in order to start the program.");
             System.out.println("Usage : node [network.conf] [node_physical_id]");
             System.exit(1);
         }
@@ -27,6 +28,14 @@ public class Node {
         // Define the ids
         physicalId = Integer.parseInt(args[1]);
         virtualId = -1;
+
+        // Usual RabbitMQ setup
+        String host = args[2];
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(host);
+
+        Connection connection;
+        Channel channel;
 
         // Read the file. We expect the following format (example) :
         /**
@@ -37,6 +46,9 @@ public class Node {
          * 0 1 1 0
          */
         try {
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+
             Scanner netConf = new Scanner(new File(args[0]));
 
             int i = netConf.nextInt();
@@ -50,13 +62,30 @@ public class Node {
                         try {
                             netTopology[m][n] = netConf.nextInt();
 
-                            if ((m == physicalId || n == physicalId) && netTopology[m][n] == 1) {
-                                // Can open a queue between our node and the remote one, depends on which is where
-                                // Shall we actually open two queues, one to each direction ?
+                            if ((m == physicalId || n == physicalId) && netTopology[m][n] == 1 && m <= n) {
+                                // Can open two queues back and forth between our node and the remote one
                                 System.out.print("Can open a queue between our node and remote one with ids ");
                                 System.out.print(m);
                                 System.out.print(" and ");
                                 System.out.println(n);
+
+                                // Open from us to the other - WRITE
+                                String fromUsToOther = Integer.toString(physicalId) + "to" + (m == physicalId ? Integer.toString(n) : Integer.toString(m));
+                                channel.queueDeclare(fromUsToOther, false, false, false, null);
+
+                                // Example
+                                channel.basicPublish("", fromUsToOther, null, ("Hello from node "+Integer.toString(physicalId)).getBytes("UTF-8"));
+
+                                // Open from other to us - READ
+                                String fromOtherToUs = (m == physicalId ? Integer.toString(n) : Integer.toString(m)) + "to" + Integer.toString(physicalId) ;
+                                channel.queueDeclare(fromOtherToUs, false, false, false, null);
+
+                                // Example 
+                                DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                                    String message = new String(delivery.getBody(), "UTF-8");
+                                    System.out.println(physicalId+" got message on queue "+fromOtherToUs+" -> "+message);
+                                };
+                                channel.basicConsume(fromOtherToUs, true, deliverCallback, consumerTag -> { });
                             }
                         } catch (Exception e) {
                             System.err.println("Error on the configuration file. Terminating...");
@@ -65,9 +94,23 @@ public class Node {
                     }
                 }
             }
+
+            // Command handler will be there
+
+            /* create_ring -> Creates the ring from the current node, with the discovery method. Doesn't work if node has already a 
+            virtual id
+
+                destroy_ring -> Just send a destroy command that resets all the virtual ids to -1, with propagation to all neighbours
+
+                send to left/right neighbour -> Only if part of a ring
+
+            */
         } catch (FileNotFoundException e) {
             System.err.println("The file "+args[0]+" was not found. This node will terminate.");
             System.exit(2);
+        } catch (Exception e) {
+            System.err.println("An error occured. This node will terminate.");
+            System.exit(4);
         }
 
     }
