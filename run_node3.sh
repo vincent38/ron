9bfdf2e22f727bd38ba3e45ce8f54eba0a317394
Olaniyan Folajimi <jimiolaniyan@gmail.com>
2021-04-15 15:48:57 +0200

    add bfs

--- run_node3.sh
+++ run_node3.sh
@@ -1,3 +1,7 @@
 export CP=.:./classes:./lib/amqp-client-5.8.0.jar:./lib/slf4j-api-1.7.30.jar:./lib/slf4j-simple-1.7.30.jar
-export CHAT_SERVER_ADDR=localhost
+server=localhost
+if [ $1 ]; then
+    server=$1
+fi
+export CHAT_SERVER_ADDR=$server
 java -cp $CP Node data/basictopology.conf 3 $CHAT_SERVER_ADDR
