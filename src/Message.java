98758b6346d04e45c153d54f2b835e1c4edb3d07
Vincent AUBRIOT <vincent3897@gmail.com>
2021-04-15 15:49:44 +0200

    Started message class

---
+++ src/Message.java
@@ -0,0 +1,13 @@
+public class Message {
+    
+    String message;
+    int rotation;
+    int iterations;
+
+    public Message(String m, int r, int i) {
+        message = m;
+        rotation = r;
+        iterations = i;
+    }
+
+}
